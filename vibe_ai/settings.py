432ecfe1e5a20929455b79e4919839431d6e17cc
designeer1 <babukalyan523@gmail.com>
2025-01-27 23:34:33 +0530

    Update settings.py

--- vibe_ai/settings.py
+++ vibe_ai/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1_b6ko1n_=ps^f(f@d8jls@(8$*u!e+c+n2@+h#xpcdo&y)h4x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
