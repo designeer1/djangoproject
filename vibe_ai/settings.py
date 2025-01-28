ee453b875e18e36d91f1f2b0998b641ed1e94c48
designeer1 <babukalyan523@gmail.com>
2025-01-28 12:27:58 +0530

    Update settings.py

--- vibe_ai/settings.py
+++ vibe_ai/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1_b6ko1n_=ps^f(f@d8jls@(8$*u!e+c+n2@+h#xpcdo&y)h4x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['djangoproject-hcbf.onrender.com']
 
 
 # Application definition
